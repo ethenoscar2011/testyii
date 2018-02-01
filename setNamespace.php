9a2fdea054dc783ff95682cc89e93995cb26b91b
lu_gui <ethenoscar2011@163.com>
2018-02-01 16:05:03 +0800

    测试

---
+++ setNamespace.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 文件描述
+ * -----------------------------------------------
+ * @copyright: http://www.zuzuche.com
+ * @author: Ethen-lugui
+ * @date: 18-2-1
+ * @time: 下午4:02
+ */
+
+echo "This is just a test!\n";
