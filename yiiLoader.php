598022c99c9fbab684bdd1193b86a27ac1867ffe
lu_gui <ethenoscar2011@163.com>
2018-02-02 17:29:33 +0800

    修复丢失部分常量的bug

--- yiiLoader.php
+++ yiiLoader.php
@@ -2,6 +2,10 @@
 
 $coreLoader = __DIR__ . '/../yiicore/yiiLoader.php';
 if (!file_exists($coreLoader)) {
+    defined('YIICORE_LOCAL') or define('YIICORE_LOCAL', true);
     defined('YIICORE_APP_BASE_PATH') or define('YIICORE_APP_BASE_PATH', __DIR__ . '/');
     defined('YIICORE_APP_VENDOR_PATH') or define('YIICORE_APP_VENDOR_PATH', YIICORE_APP_BASE_PATH . 'vendor');
+} else {
+    defined('YIICORE_LOCAL') or define('YIICORE_LOCAL', false);
+    require $coreLoader;
 }
