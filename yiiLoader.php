7ec6ec18fe808c289d181b838a95a5b533db5094
lu_gui <ethenoscar2011@163.com>
2018-02-06 15:51:34 +0800

    修复默认是测试环境的bug

--- yiiLoader.php
+++ yiiLoader.php
@@ -3,7 +3,7 @@
 $coreLoader = __DIR__ . '/../yiicore/yiiLoader.php';
 if (!file_exists($coreLoader)) {
     defined('YIICORE_LOCAL') or define('YIICORE_LOCAL', true);
-    define('YII_ENV', 'test');
+    define('YII_ENV', 'dev');
     defined('YII_DEBUG') or define('YII_DEBUG', true);
     defined('YIICORE_APP_BASE_PATH') or define('YIICORE_APP_BASE_PATH', __DIR__ . '/');
     defined('YIICORE_APP_VENDOR_PATH') or define('YIICORE_APP_VENDOR_PATH', YIICORE_APP_BASE_PATH . 'vendor');
